3313dba7e5ed843e1af23172dc83dba86aa5cfba
Ennnvvvy <deposoyivanrey_bsit@plmun.edu.ph>
2023-12-26 17:41:36 +0800

    added comments

--- src/main.js
+++ src/main.js
@@ -14,47 +14,50 @@ function imageToTable(imagePath, cellSize) {
     // Get the container div where the table will be placed
     const tableContainer = document.getElementById("tableContainer");
 
-    // Set the canvas size to match the image size
-    canvas.width = 150; // Adjusted for 100x100 resolution
-    canvas.height = 110; // Adjusted for 100x100 resolution
+    // Set the canvas to custom dimensions
+    canvas.width = 150; 
+    canvas.height = 110;
 
     // Draw the image on the canvas
-    context.drawImage(image, 0, 0, 150, 110); // Adjusted for 100x100 resolution
+    context.drawImage(image, 0, 0, 150, 110); // adjust image size similar to canvas size
 
     // Generate the HTML table with checkboxes
     let tableHTML = "<table>";
     for (let y = 0; y < 110; y += cellSize) {
-      // Adjusted for 100x100 resolution
       tableHTML += "<tr>";
       for (let x = 0; x < 150; x += cellSize) {
-        // Adjusted for 100x100 resolution
         // Get the pixel data at the current position
         const pixelData = context.getImageData(x, y, 1, 1).data;
+
         // Check if the pixel is black (assuming black is [0, 0, 0])
         const isBlack =
           pixelData[0] === 0 && pixelData[1] === 0 && pixelData[2] === 0;
 
-        // Add a table cell with a checkbox and a label
+        // Add a table cell with a checkbox
+        // set "checked" attribute if the pixel is black
         tableHTML += `
-                       <td>
-                          <input type="checkbox" id="checkbox_${x}_${y}" ${
-          isBlack ? "checked" : ""
-        } />
-                          <!-- <label for="checkbox_${x}_${y}" style="background-color: ${
-          isBlack ? "#333" : "#fff"
-        };"></label> -->
-                       </td>`;
+          <td>
+            <input type="checkbox" id="checkbox_${x}_${y}" ${ isBlack ? "checked" : "" } />
+          </td>`;
       }
       tableHTML += "</tr>";
     }
     tableHTML += "</table>";
 
+    // Add the table to the container div
     tableContainer.innerHTML = tableHTML;
   };
 }
 
 function startRender() {
   console.log("start");
+
+  // you can adjust the frame rate here
+  // 6 = 2 frames per second
+  // 12 = 4 frames per second
+  // 18 = 6 frames per second
+  // 24 = 8 frames per second
+  // and so on...
 
   // Define the frame rate and calculate the delay for each frame
   const frameRate = 6; // frames per second
@@ -74,7 +77,7 @@ function startRender() {
         }
 
         const imagePath = `./public/images/frames/frame-${paddedFrame}.jpg`;
-        const cellSize = 4; // Adjusted for 100x100 resolution
+        const cellSize = 4;
         imageToTable(imagePath, cellSize);
       }, delayPerFrame * currentFrame);
     })(frame);
@@ -83,6 +86,8 @@ function startRender() {
   }
 }
 
+// This is what is rendered on document ready
+// It's just a button to start the rendering
 function renderStartButton() {
   const tableContainer = document.getElementById("tableContainer");
   const tableHTML = `
